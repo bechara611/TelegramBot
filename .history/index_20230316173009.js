57c46ecdc303dedecda81e7821dda34c33b8a52b
Dany Bechara <bechara611@gmail.com>
2023-03-16 17:31:31 -0400

    f

---
+++ .history/index_20230316173009.js
@@ -0,0 +1,39 @@
+
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+
+export class Server {
+
+
+    constructor() {
+        this.app = express();
+        this.rutas = {
+            dany: '/dany',
+            usuarios: '/api/auth',
+            eventos:'/api/eventos'
+        }
+        this.port = process.env.PORT || '8080'
+        this.Middlewares();
+        this.Rutas();
+        this.bd();
+    }
+
+    Middlewares() {
+        this.app.use(express.static('public'))
+        this.app.use(cors())
+        this.app.use(express.json())
+    }
+    Rutas() {
+       
+    }
+
+    activarBack() {
+        this.app.listen(this.port, () => {
+            console.log(`APP running in ${this.port} `)
+        })
+    }
+    async bd(){
+        ConectarDB()
+    }
+}
